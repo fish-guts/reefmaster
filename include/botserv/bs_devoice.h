6f35776bfc0f93b85fa888e0a655f32393fb9a47
Fish-Guts <severin.mueller@gmx.net>
2014-12-26 01:40:00 +0100

    implementation of bs_devoice

---
+++ include/botserv/bs_devoice.h
@@ -0,0 +1,6 @@
+#ifndef BS_DEVOICE_H_
+#define BS_DEVOICE_H_
+
+void bs_devoice(char *src,int ac,char **av);
+
+#endif /* BS_DEVOICE_H_ */
---
+++ src/botserv/bs_devoice.c
@@ -0,0 +1,62 @@
+/*
+ *      bs_dehalfop.c - severin
+ *
+ *      Copyright (c) 2014 Severin Mueller <severin.mueller@reefmaster.org>
+ *
+ *      This program is free software; you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation; either version 2 of the License, or
+ *      (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with this program; if not, write to the Free Software
+ *      Foundation, Inc., 51 Franklin Street, Fifth Floor, alBoston,
+ *      MA 02110-1301, USA.
+ */
+
+#include "main.h"
+
+void bs_devoice(char *src,int ac,char **av) {
+	notice(as_name,src,"av[0]: %s",av[0]);
+	notice(as_name,src,"av[1]: %s",av[1]);
+	notice(as_name,src,"av[2]: %s",av[2]);
+	notice(as_name,src,"av[3]: %s",av[3]);
+	user *u = finduser(src);
+	char *botname;
+	char *chan;
+	char *nick;
+
+	if(ac<3) {
+		notice(bs_name,src,BS_ERR_DEHALFOP_USAGE);
+		notice(bs_name,src,BS_RPL_HLP,bs_name,"DEHALFOP");
+		return;
+	}
+	botname = sstrdup(av[1]);
+	chan = sstrdup(av[2]);
+	nick = sstrdup(av[3]);
+
+	bot *b = findbot(botname);
+
+	if(!b) {
+		notice(bs_name,src,BS_ERR_NOTFOUND,botname);
+		return;
+	}
+
+	if(!is_bot_on_chan(botname,chan)) {
+		notice(bs_name,src,BS_ERR_OP_NOT_ON_CHAN,botname,chan);
+		return;
+	}
+	if(!bot_identified(u,b)) {
+		notice(bs_name,src,BS_ERR_ACCESSDENIED,b->name,bs_name);
+		return;
+	}
+	devoice(b->name,nick,chan);
+
+	return;
+
+}
